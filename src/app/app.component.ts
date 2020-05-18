e30a822dd107948367784259e73b1bde3088d138
joelalive <joelimalive1994@gmail.com>
2020-05-18 21:50:15 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Server } from './server.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'max-test';
+
+  servers: Server[] = [
+    new Server('Dummy Server')
+  ];
+
+  newServer = 'Server Name';
+
+  addServer(serverName) {
+    console.log('Server Added');
+    this.servers.push({
+      name: serverName.value
+    });
+    this.newServer = serverName.value;
+  }
+
+  removeServer(index) {
+    this.servers.splice(index, 1);
+  }
+
+
 }
